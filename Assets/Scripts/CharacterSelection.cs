2e3caf2de803ee4b48e0c0f7f7ca7e1e667a9d2c
NicolasG12 <naguevar@ucsc.edu>
2022-11-29 11:18:36 -0800

    Updated the UI and now shows the health and confidence

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -8,28 +8,22 @@ using UnityEngine.UIElements;
 public class CharacterSelection : MonoBehaviour
 {
     //List to hold characters (when placed should be in the order that characters are set up)
-    //public VisualTreeAsset gameUI;
-    //public StyleSheet[] styles;
     public List<GameObject> characters;
     public GameObject enemy;
-    private VisualElement frame;
+    private VisualElement Buttons;
+    private VisualElement Health;
     //variables to hold each of the buttons
     private Button clockwise;
     private Button counterclockwise;
-    //private Button mecha;
-    //private Button magical_girl;
-    //private Button super_sentai;
     private Button attack;
     private Button confidenceAttack;
+    private ProgressBar playerHealth;
+    private ProgressBar confidence;
+    private ProgressBar enemyHealth;
     //Vectors to hold the location of the front and swapping character
-    private Vector3 front;
-    private Vector3 target;
     //holds which character is in front
-    private GameObject inFront;
+    private int inFrontSlot = 0;
     private bool moveState = false;
-    private int currentChar = -1;
-    [SerializeField]
-    private float inFrontHealth;
 
     public float speed = 0.01f;
 
@@ -38,21 +32,19 @@ public class CharacterSelection : MonoBehaviour
         var uiDocument = GetComponent<UIDocument>();
         var rootVisualElement = uiDocument.rootVisualElement;
         //create the buttons and attach them to a function when they are clicked
-        frame = rootVisualElement.Q<VisualElement>("Frame");
-        clockwise = frame.Q<Button>("clockwise");
+        Buttons = rootVisualElement.Q<VisualElement>("Buttons");
+        clockwise = Buttons.Q<Button>("clockwise");
         clockwise.RegisterCallback<ClickEvent>(ev => SwapChar(0));
-        counterclockwise = frame.Q<Button>("counter-clockwise");
+        counterclockwise = Buttons.Q<Button>("counter-clockwise");
         counterclockwise.RegisterCallback<ClickEvent>(ev => SwapChar(1));
-        //mecha = frame.Q<Button>("MechaGirl");
-        //mecha.RegisterCallback<ClickEvent>(ev => SwapChar(0));
-        //magical_girl = frame.Q<Button>("MagicalGirl");
-        //magical_girl.RegisterCallback<ClickEvent>(ev => SwapChar(1));
-        //super_sentai = frame.Q<Button>("SuperSentai");
-        //super_sentai.RegisterCallback<ClickEvent>(ev => SwapChar(2));
-        attack = frame.Q<Button>("Attack");
+        attack = Buttons.Q<Button>("Attack");
         attack.RegisterCallback<ClickEvent>(ev => Attack());
         //confidenceAttack = frame.Q<Button>("ConfidenceAttack");
         //confidenceAttack.RegisterCallback<ClickEvent>(ev => ConfidenceAttack());
+        Health = rootVisualElement.Q<VisualElement>("Health");
+        playerHealth = Health.Q<ProgressBar>("health");
+        confidence = Health.Q<ProgressBar>("confidence");
+        enemyHealth = Health.Q<ProgressBar>("enemy-health");
     }
 
     float enemyAttackPower = 0f;
@@ -64,9 +56,6 @@ public class CharacterSelection : MonoBehaviour
     void Start()
     {
         //assign the infront to the first character
-        front = characters[0].transform.position;
-        inFront = characters[0];
-        inFrontHealth = characters[0].GetComponent<CharState>().health;
         enemy.GetComponent<EnemyState>().generateAttack(ref enemyAttackPower, ref enemyAttackType);
         Debug.Log(enemy.name+"\'s gonna use "+enemyAttackType+" next turn!"+"           ignore this number: "+Random.Range(0f, 100f).ToString());
     }
@@ -74,21 +63,9 @@ public class CharacterSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (moveState != -1) {
-        //    //moves the characters to their respective positions
-        //    //while (Vector3.Distance(inFront.transform.position, target) > 0f)
-        //    //{
-        //        inFront.transform.position = Vector3.MoveTowards(inFront.transform.position, target, speed);
-        //    //}
-        //    //while (Vector3.Distance(characters[moveState].transform.position, front) > 0f) {
-        //        characters[moveState].transform.position = Vector3.MoveTowards(characters[moveState].transform.position, front, speed);
-        //    //}
-        //    if ((Vector3.Distance(inFront.transform.position, target) == 0f) && (Vector3.Distance(characters[moveState].transform.position, front)) == 0f) {
-        //        inFront = characters[moveState];
-        //        moveState = -1;
-        //    }
-        //    //update the infront character
-        //}
+        playerHealth.value = characters[inFrontSlot].GetComponent<CharState>().health;
+        confidence.value = characters[inFrontSlot].GetComponent<CharState>().confidence;
+        enemyHealth.value = enemy.GetComponent<EnemyState>().health;
         if (moveState)
         {
             foreach (var character in characters)
@@ -99,40 +76,36 @@ public class CharacterSelection : MonoBehaviour
                     moveState = false;
                 }
             }
+
         }
 
     }
     //swaps two characters based on the button that was pushed
     private void SwapChar(int data)
     {
-        //currentChar = data;
-        //if (moveState == -1 && characters[data].GetComponent<CharState>().health > 0) {
-        ////set the target to whichever character is chosen
-        //    target = characters[data].transform.position;
-        //    
-        //    moveState = data;
-        //}
         moveState = true;
- 
+        int last = characters.Count - 1;
+
         switch (data)
         {
             case 0:
-                for(int i = 0; i <= 2; i++)
+                for(int i = 0; i <= last; i++)
                 {
                     if (i == 0)
                     {
-                        characters[i].GetComponent<CharState>().target = characters[2].transform.position;
+                        characters[i].GetComponent<CharState>().target = characters[last].transform.position;
                     }
                     else
                     {
                         characters[i].GetComponent<CharState>().target = characters[i - 1].transform.position;
                     }
                 }
+                inFrontSlot = (inFrontSlot + 1) % characters.Count;
                 break;
             case 1:
-                for(int i = 0; i <=2; i++)
+                for(int i = 0; i <=last; i++)
                 {
-                    if(i == 2)
+                    if(i == characters.Count - 1)
                     {
                         characters[i].GetComponent<CharState>().target = characters[0].transform.position;
                     }
@@ -141,6 +114,7 @@ public class CharacterSelection : MonoBehaviour
                         characters[i].GetComponent<CharState>().target = characters[i + 1].transform.position;
                     }
                 }
+                inFrontSlot = Mathf.Abs((inFrontSlot + last) % characters.Count);
                 break;
             default:
                 break;
@@ -155,18 +129,18 @@ public class CharacterSelection : MonoBehaviour
     {
         if (enemy.GetComponent<EnemyState>().health <= 0f) return;
         //enemy.GetComponent<EnemyState>().generateAttack(ref enemyAttackPower, ref enemyAttackType);
-        inFront.GetComponent<CharState>().generateAttack(ref characterAttackPower, ref characterAttackType);
-        Debug.Log(inFront.name + " used "+ characterAttackType+"            ignore this number: "+Random.Range(0f, 100f).ToString());
+        characters[inFrontSlot].GetComponent<CharState>().generateAttack(ref characterAttackPower, ref characterAttackType);
+        Debug.Log(characters[inFrontSlot].name + " used "+ characterAttackType+"            ignore this number: "+Random.Range(0f, 100f).ToString());
         Debug.Log(enemy.name + " used "+enemyAttackType+"            ignore this number: "+Random.Range(0f, 100f).ToString());
-        inFront.GetComponent<CharState>().StatUpdate(ref enemyAttackPower, ref enemyAttackType);
+        characters[inFrontSlot].GetComponent<CharState>().StatUpdate(ref enemyAttackPower, ref enemyAttackType);
         enemy.GetComponent<EnemyState>().StatUpdate(ref characterAttackPower, ref characterAttackType);
-        Debug.Log(inFront.name+": "+inFront.GetComponent<CharState>().health.ToString()+"/"+inFront.GetComponent<CharState>().confidence.ToString()+"            ignore this number: "+Random.Range(0f, 100f).ToString());
+        Debug.Log(characters[inFrontSlot].name+": "+characters[inFrontSlot].GetComponent<CharState>().health.ToString()+"/"+characters[inFrontSlot].GetComponent<CharState>().confidence.ToString()+"            ignore this number: "+Random.Range(0f, 100f).ToString());
         Debug.Log(enemy.name+": "+enemy.GetComponent<EnemyState>().health.ToString()+"            ignore this number: "+Random.Range(0f, 100f).ToString());
         Debug.Log("============"+"           ignore this number: "+Random.Range(0f, 100f).ToString());
         enemy.GetComponent<EnemyState>().generateAttack(ref enemyAttackPower, ref enemyAttackType);
         Debug.Log(enemy.name+"\'s gonna use "+enemyAttackType+" next turn!"+"           ignore this number: "+Random.Range(0f, 100f).ToString()); 
         
-        if (inFront.GetComponent<CharState>().health <= 0f) Die();
+        if (characters[inFrontSlot].GetComponent<CharState>().health <= 0f) Die();
         if (enemy.GetComponent<EnemyState>().health <= 0f) {
             Debug.Log("enemy "+enemy.name+" has been defeated!");
         }
@@ -174,7 +148,7 @@ public class CharacterSelection : MonoBehaviour
 
     private void Die() {
         bool everybodyDead = false;
-        Debug.Log(inFront.name+" died!");
+        Debug.Log(characters[inFrontSlot].name+" died!");
         int nextChar = -1;
         if (characters[0].GetComponent<CharState>().health > 0) nextChar = 0;
         else if (characters[1].GetComponent<CharState>().health > 0) nextChar = 1;
