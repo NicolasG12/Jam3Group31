e2508b77f3f0af2f68dab2ea4f93b4ac72130ab3
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2022-11-30 11:45:54 -0800

    I think this is partial title screen changes

---
+++ Assets/Scripts/Start Screen/StartButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+
+    public GameObject self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void test() {
+        Debug.Log("start button clicked");
+    }
+}
